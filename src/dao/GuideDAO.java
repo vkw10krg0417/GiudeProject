60bb806ba50bedddbac800cda6efd1f904054bbd
samuelis419 <samuelis419@gmail.com>
2016-10-19 15:41:02 +0900

    3

--- src/dao/GuideDAO.java
+++ src/dao/GuideDAO.java
@@ -1,7 +1,41 @@
 package dao;
 
-public class GuideDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class GuideDAO {
+	private SqlSessionFactory factory = MybatisConfig.getSqlSessionFactory();
+	private SqlSession sqlsession;
+	
+	public GuideDAO() {}
+	
+	public Guide showGuideProfile(String member_id) {
+		Guide guide = null;
+		try{
+			sqlsession = factory.openSession();
+			guide = sqlsession.selectOne("guide.showGuideProfile", member_id);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlsession.close();
+		}
+		return guide;
+	}
+	
+	public void showGuideProtfolio(String member_id) {
+		List<Portfolio> portfolioList = null;
+		try{
+			sqlsession = factory.openSession();
+			portfolioList = sqlsession.selectList("guide.showGuidePortfolio", member_id);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlsession.close();
+		}
+		return portfolioList;
+	}
 	
 	
 }
